ce4539d7e3932d0063191b7667f7c88785a80b2c
Elvin <ttop34@icloud.com>
2017-10-10 14:44:11 -0500

    add values to albums table in database

--- albums_migration.sql
+++ albums_migration.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS albums(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist VARCHAR(50) NOT NULL,
     name  VARCHAR(100) NOT NULL,
-    release_date DATE NOT NULL,
+    release_date YEAR(4) NOT NULL,
     sales FLOAT NOT NULL,
     genre VARCHAR(100),
     PRIMARY KEY (id)
